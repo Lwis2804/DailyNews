b8b12968ea1536fb191deb86f32baaf9f7acd337
Lwis2804 <luis.enrique.enc@gmail.com>
2024-10-17 17:35:18 -0600

    [ADD] se agrega celda y table view

--- DailyNews/ViewController.swift
+++ DailyNews/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tablaNoticias: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tablaNoticias.dataSource = self
+        tablaNoticias.delegate = self
     }
 
 
 }
 
+
+
+extension ViewController : UITableViewDelegate & UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tablaNoticias.dequeueReusableCell(withIdentifier: "celdaNoticia", for: indexPath)
+        cell.textLabel?.text = "Articulo 1 "
+        return cell
+    }
+    
+    
+}
