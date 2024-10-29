deb854fdcad48a0d5fbe228ff273b3d4d60716f4
Lwis2804 <luis.enrique.enc@gmail.com>
2024-10-28 18:50:41 -0600

    [ADD] llamado de servicio iniciado

---
+++ DailyNews/NoticiasModelo.swift
@@ -0,0 +1,8 @@
+//
+//  NoticiasModelo.swift
+//  DailyNews
+//
+//  Created by LUIS GONZALEZ on 17/10/24.
+//
+
+import Foundation
---
+++ DailyNews/NoticiasWebService.swift
@@ -0,0 +1,8 @@
+//
+//  NoticiasWebService.swift
+//  DailyNews
+//
+//  Created by LUIS GONZALEZ on 17/10/24.
+//
+
+import Foundation
